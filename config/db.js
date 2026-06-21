8d0d7223d7a0c59a3e53605ad65be02baf581477
Mohd Saad Khan <mohdsaadkhan073@gmail.com>
2026-06-21 13:10:11 +0530

    Initial Commit

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try
+    {
+        console.log("Connecting to MongoDB...");
+        const conn = await mongoose.connect(process.env.MONGO_URL, {
+            serverSelectionTimeoutMS: 5000
+        });
+        console.log(`MongoDB Connected: ${conn.Connection.host}`);
+        return conn;
+    }
+    catch(error)
+    {
+        console.log("Error while connecting to MongoDB...");
+        console.log("Server will continue without database");
+    }
+}
+
+module.exports = connectDB;
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const connectDB = require("./config/db");
+const app = express();
+
+connectDB().catch(err => console.log("DB Connection Failed: ", err));
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server Running on Port ${PORT}`)
+});
